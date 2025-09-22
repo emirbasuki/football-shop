3e7bb04eee3988529f24b88b73279eb8dba5815d
Emir Fadhil Basuki <emir.fadhil41@ui.ac.id>
2025-09-22 13:46:00 +0700

    Tugas 4

--- run_station/settings.py
+++ run_station/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "emir-fadhil41-footballshop.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id/emir.fadhil41/footballshop"
+    "https://emir-fadhil41-footballshop.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
